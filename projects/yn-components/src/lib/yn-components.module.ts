37d0b62401d84240daf7b64d9f1fefd608a65c9c
Israel Vázquez <israelvazquezmorales@gmail.com>
2019-11-13 14:45:00 -0600

    Updates

--- projects/yn-components/src/lib/yn-components.module.ts
+++ projects/yn-components/src/lib/yn-components.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { NavbarComponent } from './navbar/navbar.component';
 
